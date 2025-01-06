e9893c70e6e492794019e96b5f4e76965929eba9
Glauber Vereda <gabavereda@gmail.com>
2025-01-06 04:23:57 -0300

    estabelecendo caminhos banco

--- projetoIntegrador/src/main/java/com/example/projetoIntegrador/controller/FuncionarioController.java
+++ projetoIntegrador/src/main/java/com/example/projetoIntegrador/controller/FuncionarioController.java
@@ -1,29 +1,40 @@
 package com.example.projetoIntegrador.controller;
 
 import com.example.projetoIntegrador.model.Funcionario;
+import com.example.projetoIntegrador.service.FuncionarioService;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+    @Autowired
+    FuncionarioService funcionarioService;
+
     private List<Funcionario> funcionarios = new ArrayList<>();
-    private int contadorId = 1;
 
     // Bloco estático com exemplos
     {
+
+        // convert para formato data
         try {
             // parsingdata
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -31,9 +42,9 @@ public class FuncionarioController {
             // parsingdata
             Date data1 = sdf.parse("15/05/2020");
             Date data2 = sdf.parse("10/12/2021");
-
-            funcionarios.add(new Funcionario(contadorId++, "Glauber Araujo", "Repositor", "400.157.157-15", data1, 2.050));
-            funcionarios.add(new Funcionario(contadorId++, "João Silva", "Gerente", "123.456.789-00", data2, 3.550));
+//
+//            funcionarios.add(new Funcionario(contadorId++, "Glauber Araujo", "Repositor", "400.157.157-15", data1, 2.050));
+//            funcionarios.add(new Funcionario(contadorId++, "João Silva", "Gerente", "123.456.789-00", data2, 3.550));
         } catch (ParseException e) {
             System.out.println("erro a converter data" + e);
         }
@@ -42,68 +53,45 @@ public class FuncionarioController {
     // Exibir a página de cadastro com a lista de funcionarios
     @GetMapping("/cadastrar-funcionario")
     public String exibirPaginaCadastro(Model model) {
-        model.addAttribute("funcionario", new Funcionario());
-        model.addAttribute("funcionarios", funcionarios);
+
+        Funcionario funcionario = new Funcionario();
+
+        model.addAttribute("listarTodosFuncionarios", funcionarioService.listarTodosFuncionarios());
+        model.addAttribute("funcionario", funcionario);
+
         return "cadastrar-funcionario";
     }
 
     // Processar o formulário de cadastro
     @PostMapping("/cadastro")
-    public String processarCadastro(@ModelAttribute Funcionario funcionario) {
-        funcionario.setId(contadorId++);
-        funcionarios.add(funcionario);
+    public String processarCadastro(@ModelAttribute("funcionario") Funcionario funcionario) {
+
+        if (funcionario.getId() == null) {
+            funcionarioService.criarFuncionario(funcionario);
+        } else {
+            funcionarioService.atualizarFuncionario(funcionario.getId(), funcionario);
+        }
+
         return "redirect:/funcionarios/cadastrar-funcionario"; // Redireciona para a mesma página para atualizar a lista
     }
 
-    // Excluir um funcionario
-    @PostMapping("/delete/{id}")
-    public String deletarFuncionario(@PathVariable int id) {
-        funcionarios.removeIf(funcionario -> funcionario.getId() == id);
-        return "redirect:/funcionarios/cadastrar-funcionario";
-    }
-
     // Editar um funcionario
-    @GetMapping("/edit/{id}")
-    public String editarFuncionario(@PathVariable int id, Model model) {
-        Funcionario funcionario = funcionarios.stream()
-                .filter(f -> f.getId() == id)
-                .findFirst()
-                .orElse(null);
-        if (funcionario == null) {
-            return "redirect:/funcionarios/cadastrar-funcionario";
-        }
+    @GetMapping("/atualizarFuncionarioForm/{id}")
+    public String atualizarFuncionarioForm(@PathVariable(value = "id") Integer id, Model model) {
+        Funcionario funcionario = funcionarioService.getFuncionarioId(id);
+
         model.addAttribute("funcionario", funcionario);
-        model.addAttribute("funcionarios", funcionarios); // Inclui a lista para manter na mesma página
-        return "/cadastrar-funcionario";
-    }
 
-    @PostMapping("/edit/{id}")
-    public String salvarEdicao(@PathVariable int id, @ModelAttribute Funcionario funcionarioAtualizado) {
-        Funcionario funcionarioExistente = funcionarios.stream()
-                .filter(f -> f.getId() == id)
-                .findFirst()
-                .orElse(null);
-        if (funcionarioExistente != null) {
-
-            funcionarioExistente.setNome(funcionarioAtualizado.getNome());
-            funcionarioExistente.setCargo(funcionarioAtualizado.getCargo());
-            funcionarioExistente.setCpf(funcionarioAtualizado.getCpf());
-            funcionarioExistente.setDataEntrada(funcionarioAtualizado.getDataEntrada());
-            funcionarioExistente.setSalario(funcionarioAtualizado.getSalario());
-        } else {
-            // Se o produto não existir, cria um novo (caso de criação)
-            funcionarioAtualizado.setId(contadorId);
-            funcionarios.add(funcionarioAtualizado);
-
-        }
         return "redirect:/funcionarios/cadastrar-funcionario";
     }
 
-    // TO DO
-//      @GetMapping("/pesquisar/{id}")
-//    public ResponseEntity<Produto> getProdutoById(@PathVariable Integer id) {
-//        Produto filme = produtoService.getProdutoId(id);
-//        return new ResponseEntity<>(produto, HttpStatus.OK);
-//
-//    }
+    // Excluir um funcionario
+    @PostMapping("/deletarFuncionario/{id}")
+    public String deletarFuncionario(@PathVariable int id) {
+        funcionarioService.deletarFuncionario(id);
+
+        return "redirect:/funcionarios/cadastrar-funcionario";
+    }
+
+
 }
