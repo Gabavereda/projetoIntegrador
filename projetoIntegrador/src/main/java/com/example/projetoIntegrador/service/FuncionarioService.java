2676fea1509cba88b037f17ce704e88d2dbea4f8
Glauber Vereda <gabavereda@gmail.com>
2025-01-06 04:10:36 -0300

    criando a classe service funcionario

--- projetoIntegrador/src/main/java/com/example/projetoIntegrador/service/FuncionarioService.java
+++ projetoIntegrador/src/main/java/com/example/projetoIntegrador/service/FuncionarioService.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.projetoIntegrador.service;
 
-/**
- *
- * @author glaub
- */
+import com.example.projetoIntegrador.model.Funcionario;
+import com.example.projetoIntegrador.model.FuncionarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FuncionarioService {
-    
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
+
+    public Funcionario criarFuncionario(Funcionario funcionario) {
+        funcionario.setId(null);
+        funcionarioRepository.save(funcionario);
+
+        return funcionario;
+
+    }
+
+    public Funcionario getFuncionarioId(Integer funcionarioId) {
+
+        return funcionarioRepository.findById(funcionarioId).orElse(null);
+
+    }
+
+    public Funcionario atualizarFuncionario(Integer funcionarioId, Funcionario funcionarioRequest) {
+
+        Funcionario funcionario = getFuncionarioId(funcionarioId);
+
+        funcionario.setNome(funcionarioRequest.getNome());
+        funcionario.setCargo(funcionarioRequest.getCargo());
+        funcionario.setDataEntrada(funcionarioRequest.getDataEntrada());
+        funcionario.setSalario(funcionarioRequest.getSalario());
+
+        funcionarioRepository.save(funcionario);
+
+        return funcionario;
+
+    }
+
+    public List<Funcionario> listarTodosFuncionarios() {
+        return funcionarioRepository.findAll();
+
+    }
+
+    public void deletarFuncionario(Integer funcionarioId) {
+        Funcionario funcionario = getFuncionarioId(funcionarioId);
+        funcionarioRepository.deleteById(funcionario.getId());
+
+    }
+
 }
