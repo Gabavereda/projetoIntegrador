25ab58f701429f4dade663b5e8ff7ff377ac5bb1
Glauber Vereda <gabavereda@gmail.com>
2025-01-06 03:58:13 -0300

    estabelecendo ENTIDADE

--- projetoIntegrador/src/main/java/com/example/projetoIntegrador/model/Fornecedor.java
+++ projetoIntegrador/src/main/java/com/example/projetoIntegrador/model/Fornecedor.java
@@ -1,8 +1,20 @@
 package com.example.projetoIntegrador.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Fornecedor")
 public class Fornecedor {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String nome;
     private String cnpj;
     private String endereco;
@@ -11,72 +23,6 @@ public class Fornecedor {
     private String email;
 
     public Fornecedor() {
-    }
-
-    public Fornecedor(int id, String nome, String cnpj, String endereco, String cep, String telefone, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.cnpj = cnpj;
-        this.endereco = endereco;
-        this.cep = cep;
-        this.telefone = telefone;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
 }
